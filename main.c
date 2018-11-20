1ada3135cedf433a3f0acd27d3f32541c827f3c3
Conner <cmccloney@hotmail.com>
2018-11-20 12:13:24 -0700

    a

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int read_sector(){
-	
-	return 1;
-}
-
-void main(){
+int main(){
 	printf("Main method\n");
+	FILE * in = fopen("test.img","rb");
+	unsigned int i, start_sector, length_sectors;
 
+	fseek(in, 0x1BE, SEEK_SET); //go to start of partition table
+
+	for(i = 0; i < 4; i++){ //read four etnries
+				
+	
+	}
 }
