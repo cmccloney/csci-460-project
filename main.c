ec3c208b97ee15d70cd42e2a52aec366f57e4d4c
Conner <cmccloney@hotmail.com>
2018-11-20 17:44:15 -0700

    added root_dir_sectors and fat_size to init

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+struct info {
+	unsigned char sectors_per_cluster;
+	unsigned int first_data_sector;
+	unsigned int total_sectors;
+	unsigned short reserved_sectors;
+	unsigned int current_sector;
+	unsigned char sector_flags;
+	unsigned int root_dir_size;
+	unsigned char buffer[512]; //512 bytes
+};
+
 struct boot_32 { //struct that defines boot sector, part II, relevant for FAT32
 	unsigned int fat_size; //starts at 36
 	unsigned short ext_flags; //2
@@ -19,7 +30,7 @@ struct boot_32 { //struct that defines boot sector, part II, relevant for FAT32
 	char type[8];
 }__attribute__((__packed__));
 
-struct boot_sector { //struct that defines boot sector, part I
+struct bpb { //struct that defines boot sector, part I
     	unsigned char jump_boot[3]; //starts at 0
     	char oemname[8]; 
     	unsigned short bytes_per_sector;
@@ -63,10 +74,6 @@ struct fat32_entry_long {
 	unsigned short name3[2];
 }__attribute__((__packed__));
 
-//#define SEQ_START 0x40
-//#define SEQ_DELETED 0x80
-//#define SEQ_MASK 0x3f
-
 #define READ_ONLY 0x01
 #define HIDDEN 0x02
 #define SYSTEM 0x04
@@ -78,6 +85,65 @@ struct fat32_entry_long {
 #define WRITE 0x02
 #define APPEND 0x04
 #define OVERWRITE 0x08
+
+struct info fat_info; //FAT info, global variable
+
+int read_sector(unsigned char *data, unsigned int sector){ //read in a sector
+	FILE *file;
+	file = fopen("fat_file","r+b");
+	fseek(file, sector*512,0); //512 bytes
+	fread(data,1,512,file);
+	fclose(file);
+	return 0;
+}
+
+int write_sector(unsigned char *data, unsigned int sector){ //write to a sector
+	FILE *file;
+	file = fopen("fat_file","r+b");
+	fseek(file,sector*512,0);
+	fwrite(data,1,512,file);
+	fclose(file);
+	return 0;
+}
+
+int fetch(unsigned int sector){ //fetch a given sector
+	if(sector == fat_info.current_sector){
+		return 0;
+	}
+	int new_sec = 0;
+	//more can be added here
+	new_sec |= read_sector(fat_info.buffer, sector); //read in sector
+	if(!new_sec){
+		fat_info.current_sector = sector;
+	}
+	return new_sec;
+}
+
+void init(){ //initialize the FAT32 structure
+	struct bpb *BPB;
+	unsigned int fat_size, root_dir_sectors; //more will be added here
+	//init fat_info for running, and read in first sector, sector zero
+	fat_info.current_sector = -1;
+	fat_info.sector_flags = 0;
+	fetch(0); //actually fetch sector 0
+
+	BPB = (struct bpb *) fat_info.buffer; //may not be necessary
+
+	if(BPB->bytes_per_sector != 512){ //check if BPB is structured right
+		printf("Error: wrong filesystem!\n");
+		return;
+	}
+
+	root_dir_sectors = ((BPB->root_max_entries * 32) + (BPB->bytes_per_sector - 1)) / 512; //check Microsoft's white paper for more info
+										//on these formulas (pg. 13-)
+	if(BPB->fat_size_16 != 0){
+		fat_size = BPB->fat_size_16;
+	}else{
+		fat_size = BPB->fat32.fat_size;
+	}
+	
+	
+}
 
 int main() {
 	return 0;   
