9b5464ba0e8c67da7d1b9b546a2d6c775a1e5dd9
Conner <cmccloney@hotmail.com>
2018-11-23 09:03:19 -0700

    finished init method

--- file_explorer.c
+++ file_explorer.c
@@ -29,22 +29,47 @@ struct fs_attr{ //information about the file system itself
   	unsigned short reserved_sector_count;
   	unsigned int fat_size;
   	char label[11];
-}__attribute__((__packed__)); //same thing here
+};
 
 void init_fs(FILE **fp, struct fs_attr *fs){ //used to initialize the information of the file system we're working with
 	//start with bytes_per_sector
 	unsigned short bytes;
-	fseek(*fp, 11, SEEK_SET);
-	fread(&bytes, 1, 2, *fp);
-	fs->bytes_per_sector = bytes;
+	fseek(*fp, 11, SEEK_SET); //point to fp object, and with an offset of 11, or any other offset defined below,
+				//using SEEK_SET which tells the function to start with beginning of file,
+				//seek the given information
+	fread(&bytes, 1, 2, *fp); //then, read the block of data from the stream into the value of bytes, or any other
+				//substituted variable defined below
+	fs->bytes_per_sector = bytes; //finally, store this value into their respective variable in the struct fs_attr
 
-	//next onto sectors_per_cluster
+	//next onto sectors_per_cluster, where we complete a similar operation
 	unsigned char secs;
 	fseek(*fp, 13, SEEK_SET);
 	fread(&secs, 1, 1, *fp);
 	fs->sectors_per_cluster = secs;
 
+	//reserved_sectors
+	unsigned short reserved;
+	fseek(*fp, 14, SEEK_SET);
+	fread(&reserved, 1, 2, *fp);
+	fs->reserved_sector_count = reserved;
 
+	//fat count
+	unsigned char fats;
+	fseek(*fp, 16, SEEK_SET);
+	fread(&fats, 1, 1, *fp);
+	fs->num_fats = fats;
+
+	//fat size
+	unsigned int size;
+	fseek(*fp, 36, SEEK_SET);
+	fread(&size, 1, 4, *fp);
+	fs->fat_size = size;
+
+	//label name
+	char name[11];
+	fseek(*fp, 71, SEEK_SET);
+	fread(&name, 1, 11, *fp);
+	strcpy(fs->label,name); //copy string over
 }
 
 int open_file(FILE **fp, char *token[]){ //used to open file
